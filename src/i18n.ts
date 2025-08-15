64d5b957b5a691e293952f71dacdd413894d0443
João Vitor <jvitor6674@gmail.com>
2025-08-15 16:23:42 -0300

    docs (imports): project structure.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import en from "../../locales/en/translation.json";
-import pt from "../../locales/pt/translation.json";
+import en from "../locales/en/translation.json";
+import pt from "../locales/pt/translation.json";
 
 i18n.use(initReactI18next).init({
   resources: {
