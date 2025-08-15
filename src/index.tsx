bc9d0c0830a4ee9050301d1a4a4d730f5272a884
João Vitor <jvitor6674@gmail.com>
2025-08-15 16:12:14 -0300

    docs (translation): correction in imports and strutucture.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import "./i18n";
 import { Desktop } from "./screens/Desktop";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <Desktop />
+    <Suspense fallback={<div>Carregando traduções…</div>}>
+      <Desktop />
+    </Suspense>
   </StrictMode>,
 );
